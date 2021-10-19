662b43a02777bde7aca21d348dbf93dac4a0951d
Gulam Faruk <golamfaruk204@gmail.com>
2021-10-19 12:04:54 +0600

    checking done

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,18 @@
 (function ($) {
-	"use strict";
+	// "use strict";
 
+	// // NAVIGATION
+
+    // $('#menuTaggler').click(function () {
+    //     $('#overlay').toggleClass('open');
+    // });
+
+    // // Toggle bar
+
+    // $('#menuTaggler').click(function () {
+    //     $('.bar').toggleClass('close');
+    // });
+
+	
 	// Add code here
 })(jQuery);
